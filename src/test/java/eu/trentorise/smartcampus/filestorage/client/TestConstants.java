f7024428c9ca3161b78780fad897e3617a4f1612
raman <raman@0fd5999a-94d0-4028-93f2-09691c278cb2>
2013-03-16 13:17:23 +0000

    License info

--- src/test/java/eu/trentorise/smartcampus/filestorage/client/TestConstants.java
+++ src/test/java/eu/trentorise/smartcampus/filestorage/client/TestConstants.java
@@ -1,12 +1,27 @@
+/*******************************************************************************
+ * Copyright 2012-2013 Trento RISE
+ * 
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ * 
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ ******************************************************************************/
 package eu.trentorise.smartcampus.filestorage.client;
 
 public class TestConstants {
 
-	static final String APPNAME = "smartcampus";
-	static final String BASEURL = "http://localhost:8280";
+	static final String APPNAME = "hackathon";
+	static final String BASEURL = "https://vas-dev.smartcampuslab.it";
 	static final String RESOURCE_NAME = "tux.jpeg";
 	static final String RESOURCE_NAME_UPDATE = "tux1.png";
-	static final String AUTH_TOKEN = "";
+	static final String AUTH_TOKEN = "6aa84b0c-4ba3-465b-93db-b23e85119ad0";
 	static final String USER_KEY = "";
 	static final String USER_SECRET = "";
 
